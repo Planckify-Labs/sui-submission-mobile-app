931e997f697fcfb9759c722824562e19aa58274a
satriaali <satrialipatullah@gmail.com>
2025-05-12 17:40:56 +0700

    update home to render mnemonic and private key

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,15 @@
-import { Stack } from "expo-router";
+import { Text, View } from "react-native";
 import { english, generateMnemonic, generatePrivateKey } from 'viem/accounts';
 
-export default function RootLayout() {
+export default function Home() {
   const privateKey = generatePrivateKey()
   console.log({privateKey})
   
   const mnemonic = generateMnemonic(english)
   console.log({mnemonic})
-  return <Stack />;
+  return <View>
+    <Text>Hello</Text>
+    <Text>{privateKey}</Text>
+    <Text>{mnemonic}</Text>
+  </View>;
 }
