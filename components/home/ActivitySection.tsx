06c77820c21aa720c8a4361ba8b89ca68a47883b
satriaali <satrialipatullah@gmail.com>
2025-07-18 18:14:04 +0700

    feat(activity-section): enhance transaction history display and address truncation
    - Integrated transaction fetching for both transfer and payment histories using `useTransactionSearch`.
    - Updated the `ActivitySection` component to dynamically render transaction buttons based on fetched data.
    - Added a utility function `truncateAddress` to format wallet addresses for better readability.

--- components/home/ActivitySection.tsx
+++ components/home/ActivitySection.tsx
@@ -1,9 +1,44 @@
+import { TTransaction } from "@/api/types/transaction";
+import { useTransactionSearch } from "@/hooks/queries/useTransactions";
+import { useWallet } from "@/hooks/useWallet";
 import { router } from "expo-router";
 import { MoveRight, Send } from "lucide-react-native";
 import React from "react";
 import { Text, TouchableOpacity, View } from "react-native";
 
 export default function ActivitySection() {
+  const { activeWallet } = useWallet();
+  const { data: transferHistory } = useTransactionSearch({
+    type: "TRANSFER",
+    senderAddress: activeWallet?.address,
+  });
+  const { data: paymentHistory } = useTransactionSearch({
+    type: "PAYMENT",
+    senderAddress: activeWallet?.address,
+  });
+
+  const purchaseHistoryButton = (payment: TTransaction) => (
+    <View key={payment.id} className="items-center justify-center">
+      <View className="bg-light-primary-red/10 rounded-xl aspect-square w-20 items-center justify-center">
+        <Send color="#c71c4b" size={35} />
+      </View>
+      <Text className="text-light-matte-black text-center text-sm font-bold">
+        Product Name
+      </Text>
+    </View>
+  );
+
+  const transferHistoryButton = () => (
+    <View className="justify-center items-center">
+      <View className="aspect-square w-full max-w-[70px] relative bg-light-primary-red/10 rounded-full items-center justify-center p-3">
+        <Text className="text-light-matte-black font-bold text-xs">88 ETH</Text>
+        <View className="bg-light-main-container aspect-square w-6 rounded-full absolute bottom-0 right-0 items-center justify-center" />
+      </View>
+      <Text className="text-light-matte-black text-center text-xs font-bold mt-1">
+        0x123...456
+      </Text>
+    </View>
+  );
   return (
     <View className="px-4">
       <View className="bg-light rounded-[14px] w-full p-[22px] gap-4">
@@ -20,32 +55,17 @@ export default function ActivitySection() {
             <MoveRight size={20} color="#c71c4b" />
           </TouchableOpacity>
         </View>
-        <View className="flex-row justify-between">
-          {Array.from({ length: 4 }).map((_, i) => (
-            <View key={i} className="justify-center items-center">
-              <View className="aspect-square w-[70px] relative bg-light-primary-red rounded-full items-center justify-center p-3">
-                <Text className="text-light font-bold text-sm">
-                  0xf322..e34fa
-                </Text>
-                <View className="bg-emerald-500 aspect-square w-6 rounded-full absolute bottom-0 right-0"></View>
-              </View>
-              <Text className="text-light-matte-black text-center text-sm font-bold">
-                ENS.eth
-              </Text>
-            </View>
-          ))}
+        <View
+          className={`flex-row ${paymentHistory && paymentHistory.length < 4 ? "justify-start gap-4" : "justify-between"}`}
+        >
+          {(paymentHistory?.length ?? 0) >= 4
+            ? paymentHistory
+                ?.slice(0, 4)
+                .map((payment) => purchaseHistoryButton(payment))
+            : paymentHistory?.map((payment) => purchaseHistoryButton(payment))}
         </View>
         <View className="flex-row justify-between gap-2">
-          {Array.from({ length: 4 }).map((_, i) => (
-            <View key={i} className="items-center justify-center">
-              <View className="bg-light-primary-red/10 rounded-xl aspect-square w-20 items-center justify-center">
-                <Send color="#c71c4b" size={35} />
-              </View>
-              <Text className="text-light-matte-black text-center text-sm font-bold">
-                Transfer
-              </Text>
-            </View>
-          ))}
+          {Array.from({ length: 4 }).map((_, i) => transferHistoryButton())}
         </View>
       </View>
     </View>
--- utils/walletUtils.ts
+++ utils/walletUtils.ts
@@ -79,3 +79,17 @@ export function createWalletFromParams(
 
   return null;
 }
+
+export function truncateAddress(
+  address: string,
+  startLength = 4,
+  endLength = 4,
+): string {
+  if (!address) return "";
+  if (address.length <= startLength + endLength) return address;
+
+  const start = address.slice(0, startLength);
+  const end = address.slice(-endLength);
+
+  return `${start}...${end}`;
+}
